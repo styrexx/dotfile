78a406da77054cc3778c6224c3ef46ae2bca851a
styrexx <pro.cyber.gr@gmail.com>
2024-06-30 15:08:13 +0100

    add installer for programm and ignorerl

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# alacritty zsh tmux nvim starship stow lf
+
+[ $(command -v alacritty) ] || echo "Downloading alacritty ..."
+apt-get install alacritty
+[ $(command -v zsh) ] || echo "Downloading zsh ..."
+apt-get install zsh
+[ $(command -v tmux) ] || echo "Downloading tmux ..."
+apt-get install tmux
+[ $(command -v stow) ] || echo "Downloading stow ..."
+apt-get install stow
+
+# installing nvim
+LINKNVIM=https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+echo "Downloading nvim ..."
+mkdir instt
+cd instt
+curl -LO $LINKNVIM
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm nvim-linux64.targz
+echo "Downloading nvim DONE"
+
+# Installing starship
+echo "Downloading starship ..."
+curl -sS https://starship.rs/install.sh | sh
