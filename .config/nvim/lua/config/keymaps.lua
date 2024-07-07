d98a48d817d58d0fc186e06c4840ef7cdf1494f2
styrexx <pro.cyber.gr@gmail.com>
2024-07-07 23:03:19 +0100

    add the keymap for faset changing one word in file and update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,3 +10,10 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.g.user_emmet_mode = "n"
+
+LazyVim.safe_keymap_set(
+  "n",
+  "<leader>sn",
+  ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
+  { desc = "Changing word in file", silent = false }
+)
